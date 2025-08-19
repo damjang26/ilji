5946eabb30dea3d13453f7f4df1eff48401f965c
Jung Hyunwook <jpwook94@gmail.com>
2025-08-19 15:04:46 +0900

    back sample commit

--- src/main/java/com/bj/ilji_server/repository/SampleRepository.java
+++ src/main/java/com/bj/ilji_server/repository/SampleRepository.java
@@ -1,4 +1,8 @@
+// src/main/java/com/bj/ilji_server/repository/SampleRepository.java
 package com.bj.ilji_server.repository;
 
-public interface SampleRepository {
+import com.bj.ilji_server.entity.SampleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
 }
