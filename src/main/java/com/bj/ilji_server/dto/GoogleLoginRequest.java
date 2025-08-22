ae78010f055f8d84f0b169eadecaeedce4c7127d
Jung Hyunwook <jpwook94@gmail.com>
2025-08-22 17:23:09 +0900

    백엔드 첫 커밋 헤헷

---
+++ src/main/java/com/bj/ilji_server/dto/GoogleLoginRequest.java
@@ -0,0 +1,10 @@
+package com.bj.ilji_server.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GoogleLoginRequest {
+    private String token;
+}
