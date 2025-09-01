edaec0a2186aba6dd9ab35bbb90a88b4bafe786c
hyeonseo <hyeonseo629@gmail.com>
2025-09-01 09:47:43 +0900

    application.

---
+++ src/main/java/com/bj/ilji_server/IljiServerApplication.java
@@ -0,0 +1,13 @@
+package com.bj.ilji_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IljiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IljiServerApplication.class, args);
+    }
+
+}
