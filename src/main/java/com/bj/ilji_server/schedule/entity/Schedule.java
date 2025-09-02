b6bcf7abc2f4f60e6d4c970207dfb71649573abc
Jung Hyunwook <jpwook94@gmail.com>
2025-09-02 10:53:13 +0900

    commit

--- src/main/java/com/bj/ilji_server/schedule/entity/Schedule.java
+++ src/main/java/com/bj/ilji_server/schedule/entity/Schedule.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Table(name = "schedules")
 @Getter
 @NoArgsConstructor
+@ToString
 public class Schedule {
 
     @Id
