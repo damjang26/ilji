6cf759ed947b31bf5fb1d7a7dfc5626af8e39653
jaehoon <jesjang705@naver.com>
2025-09-12 18:31:15 +0900

    update(nickname check)

---
+++ src/main/java/com/bj/ilji_server/user_profile/dto/UserProfileNicknameSetRequest.java
@@ -0,0 +1,10 @@
+package com.bj.ilji_server.user_profile.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserProfileNicknameSetRequest {
+    private String nickname;
+}
