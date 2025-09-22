ad64ec9c20cd47611bc215c4a75709bd714a6caa
Jung Hyunwook <jpwook94@gmail.com>
2025-09-22 14:38:07 +0900

    오랜만에 커밋

--- src/main/java/com/bj/ilji_server/chat/ChatRoomRepository.java
+++ src/main/java/com/bj/ilji_server/chat/ChatRoomRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
 
     // JPQL 쿼리를 사용하여 특정 사용자가 참여한 모든 채팅방을 찾습니다.
-    @Query("SELECT cr FROM ChatRoom cr JOIN cr.participants p WHERE p.user.id = :userId")
+    @Query("SELECT cr FROM ChatRoom cr JOIN cr.participants p WHERE p.user.id = :userId AND p.isActive = true ORDER BY cr.createdAt DESC")
     List<ChatRoom> findChatRoomsByUserId(@Param("userId") Long userId);
 }
