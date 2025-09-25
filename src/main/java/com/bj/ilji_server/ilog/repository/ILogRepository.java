29d6a3e154d5b7619f0a4c8f8ab7ad062c209476
choiayeong <5621997a@gmail.com>
2025-09-25 18:03:00 +0900

    Chang for I-Log in Mobile (모바일에서 쓰는 거 지우면 슬퍼요...)

--- src/main/java/com/bj/ilji_server/ilog/repository/ILogRepository.java
+++ src/main/java/com/bj/ilji_server/ilog/repository/ILogRepository.java
@@ -32,9 +32,19 @@ public interface ILogRepository extends JpaRepository<ILog, Long>  {
             @Param("endDate") LocalDate endDate
     );
 
+    // 모바일에서 사용 중입니다. //
     // ✅ [개선] N+1 문제 해결을 위해 JOIN FETCH를 사용하여 ILog와 UserProfile을 한 번의 쿼리로 조회합니다.
-//    @Query("SELECT i FROM ILog i JOIN FETCH i.userProfile WHERE i.userProfile.userId = :userProfileId ORDER BY i.logDate ASC")
-//    List<ILog> findAllByUserProfileUserIdWithUserProfile(@Param("userProfileId") Long userProfileId);
+    @Query("SELECT i FROM ILog i JOIN FETCH i.userProfile WHERE i.userProfile.userId = :userProfileId ORDER BY i.logDate ASC")
+    List<ILog> findAllByUserProfileUserIdWithUserProfile(@Param("userProfileId") Long userProfileId);
+
+    // 모바일에서 사용 중입니다. //
+    // 이전 일기 (현재 날짜보다 작은 것 중 제일 최근 것 하나)
+    Optional<ILog> findFirstByUserProfileUserIdAndLogDateLessThanOrderByLogDateDesc(Long userProfileUserId, LocalDate logDate);
+
+    // 모바일에서 사용 중입니다. //
+    // 다음 일기 (현재 날짜보다 큰 것 중 제일 가까운 것 하나)
+    Optional<ILog> findFirstByUserProfileUserIdAndLogDateGreaterThanOrderByLogDateAsc(Long userProfileUserId, LocalDate logDate);
+
 
     @Query("SELECT i FROM ILog i JOIN FETCH i.userProfile WHERE i.userProfile.userId = :userProfileId AND i.logDate = :logDate")
     Optional<ILog> findByUserProfileUserIdAndLogDate(@Param("userProfileId") Long userProfileId, @Param("logDate") LocalDate logDate);
